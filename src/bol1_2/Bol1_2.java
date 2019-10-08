adbce2c04f97a04df5b579dd1619cadc1db43ded
Jose Alonso <jalonsoriveiro@danielcastelao.org>
2019-10-08 08:55:06 +0200

    Inicio

---
+++ src/bol1_2/Bol1_2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol1_2;
+
+/**
+ *
+ * @author jalonsoriveiro
+ */
+public class Bol1_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Realiza un ordinograma que permita calcular o área
+        //dun cadrado de 3m de lado.De seguido crea un proxecto 
+        //en java, co nome boletin1_2 , para executalo
+        
+        float lado = 3f;
+        float area = lado * lado;
+        System.out.println("Area del cuadrado: " +area+" Con "+lado+" de lado");
+        
+    }
+    
+}
